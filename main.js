bacc9cde5c40ad39a648004c50e6d78b342af84a
susanmaharjan <530756@nwmissouri.edu>
2020-03-02 11:33:33 -0600

    1st commit

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 const add = (x, y) => { return x + y }
 
-const validate = async (event) => {
-  console.log(`triggered validate on ${event.target.id}`)
-  const isValid = event.target.checkValidity()
-  if (isValid) {
-    event.target.nextElementSibling.innerHTML = ''
-  } else {
-    event.target.nextElementSibling.innerHTML = 'Invalid input'
-    event.target.focus()
-  }
-}
+
 
 const updateWithAdd = async (event) => {
   document.querySelector('#result').innerHTML = ''
@@ -21,31 +12,7 @@ const updateWithAdd = async (event) => {
     const ans = `${s}, your sum is ${add(i, j)}.`
     document.querySelector('#result').innerHTML = ans
   }
-}
-
-const updateWithJoke = async (event) => {
-  document.querySelector('#result').innerHTML = ''
-  const url = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
-  const response = await fetch(url)
-  const obj = await response.json()
-  const joke = obj.value.joke || 'No joke for you.'
-  document.querySelector('#result').innerHTML = joke
-}
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
-
-document.addEventListener('focusout', event => {
-  if ((event.target && event.target.id === 'firstNumber') ||
-    (event.target && event.target.id === 'secondNumber')) {
-    validate(event)
-  }
-})
 
 document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'addButton') { updateWithAdd(event) }
-})
-
-document.addEventListener('click', event => {
-  if (event.target && event.target.id === 'getJokeButton') { updateWithJoke(event) }
-})
+  if (event.target && event.target.id === 'length') { updatelength(event) }
+}
